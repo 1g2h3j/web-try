c5f3ed41e712196a89796a23ac76776973b7f8a4
1g2h3j <affannnmaull99@gmail.com>
2022-07-26 23:09:10 +0700

    delete & add some files

---
+++ pages/example.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div class="md:container md:mx-auto">
+      <div class="columns-3xs">
+        <img class="w-full aspect-video" src="/img/examp-1.jpg" />
+        <img class="w-full aspect-square" src="/img/examp-2.jpg" />
+        <!-- ... -->
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
